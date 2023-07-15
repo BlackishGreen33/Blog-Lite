9c42ff3d143a0b43144ab6d1d7f33fa8067b9fbf
墨綠BG <s5460703@gmail.com>
2023-07-15 22:13:19 +0800

    [fix] cannot read properties of undefined (reading 'map')

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home({ posts }) {
 		<div className="container mx-auto px-10 mb-8">
 			<div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
 				<div className="lg:col-span-8 col-apan-1">
-					{posts.map((post) => <PostCard post={post} key={post.title} />)}
+					{posts?.map((post) => <PostCard post={post} key={post.title} />)}
 				</div>
 				<div className="lg:col-span-4 col-span-1">
 					<div className="lg:sticky relative top-8">
